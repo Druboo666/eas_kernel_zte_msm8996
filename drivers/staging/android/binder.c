741e57722d8c6b29f6e492e141bae029e25d6512
OrdenKrieger <OrdenKriegerEDR@googlemail.com>
2017-06-25 14:02:45 +0200

    gcc: Fix more code errors detected using Linaro GCC 6.3.1
    Performed a build with the latest Linaro GCC toolchain (6.3.1-2017.02)
    explicitly to take advantage of improved code error detection. These
    appeared as warnings from the compiler. This commit works in addition with sultans commit 3a14b35.
    
    Signed-off-by: OrdenKrieger <OrdenKriegerEDR@googlemail.com>

--- drivers/staging/android/binder.c
+++ drivers/staging/android/binder.c
@@ -2426,8 +2426,9 @@ retry:
 			death = container_of(w, struct binder_ref_death, work);
 			if (w->type == BINDER_WORK_CLEAR_DEATH_NOTIFICATION)
 				cmd = BR_CLEAR_DEATH_NOTIFICATION_DONE;
-			else
+			else {
 				cmd = BR_DEAD_BINDER;
+			}
 				if (put_user_preempt_disabled(cmd, (uint32_t __user *) ptr))
 				return -EFAULT;
 			ptr += sizeof(uint32_t);
